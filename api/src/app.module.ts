528fa756e4bd3bc925d3887d9b328d4f675df938
NicolasDrp <drapier.nicolas07@gmail.com>
2025-05-08 16:21:53 +0200

    feat(api): integrate TypeORM with PostgreSQL and add Swagger documentation to the main.ts file

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MessageModule } from './message/message.module';
+import { Message } from './message/entities/message.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'user',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'mfa',
+      entities: [Message],
+      synchronize: true,
+    }),
+    MessageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
