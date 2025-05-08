647be8bda50e05f33598090a41081b2a930bf52e
NicolasDrp <drapier.nicolas07@gmail.com>
2025-05-08 17:17:38 +0200

    feat(message): add sender and content properties to UpdateMessageDto Swagger documentation

--- api/src/message/dto/update-message.dto.ts
+++ api/src/message/dto/update-message.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMessageDto } from './create-message.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateMessageDto extends PartialType(CreateMessageDto) {}
+export class UpdateMessageDto extends PartialType(CreateMessageDto) {
+  @ApiProperty({ example: 'NouveauPseudo' })
+  sender?: string;
+
+  @ApiProperty({ example: 'Nouveau contenu du message' })
+  content?: string;
+}
