058aa01e9b92c9c745db5622cd6d389d038bfb5a
NicolasDrp <drapier.nicolas07@gmail.com>
2025-05-12 14:40:15 +0200

    feat: add HTML and CSS for message display (thread)

--- thread/server.js
+++ thread/server.js
@@ -1,24 +1,45 @@
+/* eslint-disable no-undef */
 const express = require("express");
 const axios = require("axios");
+const path = require("path");
+const fs = require("fs");
 const app = express();
 const PORT = 80;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Function to read HTML template
+const getHtmlTemplate = () => {
+  return fs.readFileSync(
+    path.join(__dirname, "public", "template.html"),
+    "utf8"
+  );
+};
 
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("http://api:3000/message");
     const messages = response.data;
 
-    const html = `
-      <h1>Messages :</h1>
-      <ul>
-        ${messages
-          .sort((a, b) => new Date(b.date) - new Date(a.date))
-          .map(
-            (m) => `<li><strong>${m.sender.pseudo}</strong> : ${m.content}</li>`
-          )
-          .join("")}
-      </ul>
-    `;
+    const messagesHtml = messages
+      .sort((a, b) => new Date(b.date) - new Date(a.date))
+      .map(
+        (m) => `<li class="message">
+          <div class="message-header">
+            <span class="sender">${m.sender.pseudo}</span>
+            <span class="date" data-date=${m.date}></span>
+          </div>
+          <div class="message-content">${m.content}</div>
+        </li>`
+      )
+      .join("");
+
+    console.log(messages);
+
+    // Replace the placeholder in the template with the actual messages
+    const template = getHtmlTemplate();
+    const html = template.replace("{{MESSAGES}}", messagesHtml);
 
     res.send(html);
   } catch (err) {
