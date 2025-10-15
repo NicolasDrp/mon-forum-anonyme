c70d6ef43352403615cb94945a665a36593f209a
NicolasDrp <drapier.nicolas07@gmail.com>
2025-10-15 15:37:32 +0200

    ci: configure-aws-credentials from repo secrets in CI

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,18 +7,6 @@ variable "aws" {
   }
 }
 
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true
-}
-
 variable "image_tag" {
   description = "Tag de l'image Docker (hash du commit)"
   type        = string
